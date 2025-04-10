40bca57e464f5583f1b5a9a2242e63881fd1650e
aitbenmoumen <a.aitbenmoumen@gmail.com>
2025-04-10 14:15:50 +0100

    relations are fixed

--- src/main/java/com/emsi/hopital/entities/Patient.java
+++ src/main/java/com/emsi/hopital/entities/Patient.java
@@ -1,12 +1,18 @@
 package com.emsi.hopital.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Collection;
 import java.util.Date;
 
+@Entity
 public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Temporal(TemporalType.DATE)
     private Date dateOfBirth;
     private boolean malade;
+    @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY)
     private Collection<RendezVous> rendezVous;
 }
--- src/main/java/com/emsi/hopital/entities/RendezVous.java
+++ src/main/java/com/emsi/hopital/entities/RendezVous.java
@@ -1,11 +1,24 @@
 package com.emsi.hopital.entities;
 
-import java.util.Date;
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class RendezVous {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date date;
     private boolean annule;
     private StatusRdv status;
+    @ManyToOne
     private Medcin medcin;
+    @ManyToOne
+    private Patient patient;
+    @OneToOne(mappedBy = "rendezVous")
+    private Consultation consultation;
 }
